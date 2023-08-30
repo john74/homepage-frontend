0004869a312fe1a7d1bb8d8b6e4a41662c0a05c3
JohnEf <eustathiou.ioan@protonmail.com>
2023-08-30 16:23:09 +0200

    set access and refresh tokens as http only cookies.set session strategy to jwt

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
-
+import { cookies } from 'next/headers'
 
 export const authOptions = {
     providers: [
@@ -8,8 +8,9 @@ export const authOptions = {
             async authorize(credentials, req) {
                 const { email, password } = credentials;
 
-                const response = await fetch(process.env.BACKEND_SIGN_IN_URL, {
+                const signInResponse = await fetch(process.env.BACKEND_SIGN_IN_URL, {
                     method: "POST",
+                    credentials: "include",
                     headers: {
                         "Content-Type": "application/json",
                     },
@@ -19,7 +20,32 @@ export const authOptions = {
                     }),
                 });
 
-                const user = await response.json();
+                const response = await signInResponse;
+                const setCookieValue = response.headers.get('set-cookie');
+
+                const accessTokenMatch = setCookieValue.match(/accessToken=([^;]+)/);
+                if (accessTokenMatch && accessTokenMatch[1]) {
+                    cookies().set({
+                        name: "accessToken",
+                        value: accessTokenMatch[1],
+                        httpOnly: true,
+                        maxAge: 60 * 30, // 30 minutes
+                        path: "/"
+                    })
+                }
+
+                const refreshTokenMatch = setCookieValue.match(/refreshToken=([^;]+)/);
+                if (refreshTokenMatch && refreshTokenMatch[1]) {
+                    cookies().set({
+                        name: "refreshToken",
+                        value: refreshTokenMatch[1],
+                        httpOnly: true,
+                        maxAge: 60 * 60 * 24 * 30, // 30 days
+                        path: "/"
+                    })
+                }
+
+                const user = response.json();
                 if (response.ok && user) return user;
                 return null;
             }
@@ -38,6 +64,9 @@ export const authOptions = {
 
     pages: {
         signIn: "/sign-in"
+    },
+    session: {
+        strategy: "jwt"
     },
 
     debug: process.env.NODE_ENV === "development",
