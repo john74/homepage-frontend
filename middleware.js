45f32ab6855acbfe37048302f5cab29512293792
JohnEf <eustathiou.ioan@protonmail.com>
2023-08-31 16:51:24 +0200

    import middlewares

--- middleware.js
+++ middleware.js
@@ -1,32 +1,9 @@
-import { NextResponse } from 'next/server';
-import { jwtDecode } from 'jwt-js-decode';
+import { stackMiddlewares } from '@middlewares/stackMiddlewares';
+import { refreshTokenValidity } from '@middlewares/refreshTokenValidity';
 
 
-export async function middleware(request) {
-    // Check if the refresh token has expired by comparing its expiration timestamp
-    // with the current timestamp. If the refresh token has expired, it cannot be
-    // used to obtain another accessToken, necessitating the user's logout.
-    // The user is redirected to the sign-in page and all cookies associated with
-    // the current session are deleted to perform the logout operation.
-    const refreshTokenCookie = request.cookies.get('refreshToken');
-    if (refreshTokenCookie) {
-        const refreshToken = refreshTokenCookie.value;
-        const refreshTokenPayload = jwtDecode(refreshToken).payload;
-        const refreshTokenExpiration = refreshTokenPayload.exp;
-        const currentTimestamp = Math.floor(Date.now() / 1000);
-
-        // Compare the two Unix timestamps to verify if the refreshToken remains valid.
-        if (refreshTokenExpiration < currentTimestamp) {
-            const response = NextResponse.redirect(new URL('/', request.url));
-
-            request.cookies.getAll().forEach(cookie => {
-                response.cookies.delete(cookie.name);
-            });
-
-            return response;
-        }
-    }
-}
+const middlewares = [refreshTokenValidity];
+export default stackMiddlewares(middlewares);
 
 
 export const config = {
