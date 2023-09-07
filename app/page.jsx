a0b473285e471487d047cbeeec967532980c2bf6
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-07 17:54:48 +0200

    feat: Retrieve the current user session and fetch data for shortcuts, bookmarks, bookmark categories and search engines.

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,22 @@
-const Home = () => {
+import { getServerSession } from "next-auth";
+
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { getData } from "@lib";
+
+import { Sidebar } from "@components";
+
+
+const HomePage = async () => {
+    let session = await getServerSession(authOptions);
+    const shortcuts = await getData(process.env.BACKEND_SHORTCUTS_URL, session);
+    const bookmarkCategories = await getData(process.env.BACKEND_BOOKMARK_CATEGORIES_URL, session);
+    const bookmarks = await getData(process.env.BACKEND_BOOKMARKS_URL, session);
+    const searchEngines = await getData(process.env.BACKEND_SEARCH_ENGINES_URL, session);
+
+
     return (
         <div>HOME PAGE</div>
     )
 }
 
-export default Home;
+export default HomePage;
