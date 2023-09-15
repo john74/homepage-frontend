4fe6180350f027cabc7f95aa7df7dd4a5adf7330
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-15 18:45:04 +0200

    feat: export modules

--- hooks/index.js
+++ hooks/index.js
@@ -1,2 +1,4 @@
 export { default as useSignUp } from './useSignUp';
 export { default as useSignIn } from './useSignIn';
+export { default as useSetDefaultSearchEngine } from './useSetDefaultSearchEngine';
+export { default as useMenuToggle } from './useMenuToggle';
