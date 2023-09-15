a10c1a1193cd9e878e2fad373f6f79715f4b1c37
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-15 18:51:48 +0200

    feat: Create 'search-engines/bulk-update' endpoint

---
+++ app/api/search-engines/bulk-update/route.js
@@ -0,0 +1,23 @@
+import { refreshAccessToken } from "@lib";
+
+
+export async function PUT(request) {
+    let accessToken = request.cookies.get("accessToken")?.value;
+    if (!accessToken) {
+      accessToken = await refreshAccessToken(request);
+    }
+
+    const data = await request.json();
+    const initOptions = {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+            authorization: `JWT ${accessToken}`,
+        },
+        body: JSON.stringify(data)
+    };
+
+    let response = await fetch(process.env.BACKEND_SEARCH_ENGINES_UPDATE_URL, initOptions);
+
+    return Response.json(response);
+}
