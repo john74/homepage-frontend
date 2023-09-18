b59990447ab248c17ff7a5385fc2960234b4e9db
JohnEf <eustathiou.ioan@protonmail.com>
2023-09-18 12:53:12 +0200

    feat: Import 'groupData' function and 'Bookmarks' component

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,11 @@ import { getServerSession } from "next-auth";
 
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { getData } from "@lib";
+import { groupData } from "@lib";
 
 import { LeftSidebar } from "@components";
 import { WebSearch } from "@components";
+import { Bookmarks } from "@components";
 
 
 const HomePage = async () => {
@@ -13,7 +15,7 @@ const HomePage = async () => {
     const bookmarkCategories = await getData(process.env.BACKEND_BOOKMARK_CATEGORIES_URL, session);
     const bookmarks = await getData(process.env.BACKEND_BOOKMARKS_URL, session);
     const searchEngines = await getData(process.env.BACKEND_SEARCH_ENGINES_URL, session);
-
+    let groupedBookmarkCategories = groupData(bookmarkCategories, 6);
 
     return (
         <div id="home">
@@ -22,6 +24,7 @@ const HomePage = async () => {
             </div>
             <div id="right">
                 <WebSearch searchEngines={searchEngines}/>
+                <Bookmarks groupedBookmarkCategories={groupedBookmarkCategories} bookmarks={bookmarks}/>
             </div>
         </div>
     )
