6a626d4f817f3de101b0a2162a35f3ce857534ae
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-05 12:14:38 +0200

    feat: Add bookmark actions menu

--- components/BookmarkCategoryBody.jsx
+++ components/BookmarkCategoryBody.jsx
@@ -1,4 +1,6 @@
 import styles from '../styles/BookmarkCategoryBody.module.css';
+import Svg from './Svg';
+
 
 function BookmarkCategoryBody(props) {
     return (
@@ -8,6 +10,14 @@ function BookmarkCategoryBody(props) {
                     <a href={bookmark.url} target="_blank" rel="noopener noreferrer">
                         {bookmark.name}
                     </a>
+                    <div className={styles.actions}>
+                        <span title="Edit">
+                            <Svg content={<><path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/><path d="m15 5 4 4"/></>}/>
+                        </span>
+                        <span title="Delete">
+                            <Svg content={<><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></>}/>
+                        </span>
+                    </div>
                 </div>
             ))}
         </div>
