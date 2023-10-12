d0fc1433a47600af3a34007191119bdd5372c533
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-12 12:17:09 +0200

    refactor: Update data retrieval for bookmarks

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const HomePage = async () => {
     let session = await getServerSession(authOptions);
     const shortcuts = await getData(process.env.BACKEND_SHORTCUTS_URL, session);
     const bookmarkCategoryGroups = (await getData(process.env.BACKEND_BOOKMARK_CATEGORIES_URL, session)).categories;
-    const bookmarks = await getData(process.env.BACKEND_BOOKMARKS_URL, session);
+    const bookmarks = (await getData(process.env.BACKEND_BOOKMARKS_URL, session)).bookmarks;
     const searchEngines = await getData(process.env.BACKEND_SEARCH_ENGINES_URL, session);
 
     const props = {
