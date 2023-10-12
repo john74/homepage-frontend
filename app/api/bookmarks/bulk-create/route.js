8e9be1c15bad7e737de0fc0bc477429e7f4d7c9b
JohnEf <eustathiou.ioan@protonmail.com>
2023-10-12 15:38:16 +0200

    refactor: Remove code for merging the newly created bookmarks with the existing bookmarks.

--- app/api/bookmarks/bulk-create/route.js
+++ app/api/bookmarks/bulk-create/route.js
@@ -8,7 +8,6 @@ export async function POST(request) {
     }
 
     const body = await request.json();
-    const formData = body.formData;
 
     const initOptions = {
         method: "POST",
@@ -16,17 +15,9 @@ export async function POST(request) {
             "Content-Type": "application/json",
             authorization: `JWT ${accessToken}`,
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(body)
     };
 
     let response = await fetch('http://127.0.0.1:8000/api/bookmarks/bulk-create/', initOptions);
-    const createdBookmarks = (await response.json()).bookmarks;
-    const bookmarks = body.bookmarks;
-    Object.keys(createdBookmarks).forEach(categoryId => {
-        let newBookmarks = createdBookmarks[categoryId];
-        newBookmarks.forEach(bookmark => {
-            bookmarks[categoryId].push(bookmark);
-        });
-    });
-    return NextResponse.json({bookmarks});
+    return NextResponse.json(await response.json());
 }
