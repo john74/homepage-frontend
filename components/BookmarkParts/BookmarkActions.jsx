a3318bec05defd66f135af780a696a0f5892f5fe
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-13 12:51:05 +0100

    feat: Implement error handling in bookmark deleting process

--- components/BookmarkParts/BookmarkActions.jsx
+++ components/BookmarkParts/BookmarkActions.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Svg from '../Svg';
+import { toast } from "react-hot-toast";
 import {
     Button,
 } from '@components';
@@ -38,11 +39,24 @@ function BookmarkActions(props) {
             'http://localhost:3000/api/bookmarks/bulk-delete/',
             initOptions
           )
+          .catch(error => {
+            return {error: error}
+          })
 
-        if (response.ok) {
-            const response_data = await response.json();
-            const bookmarks = response_data.bookmarks;
-            const shortcuts = response_data.shortcuts;
+        if (response?.error || response?.status == 500) {
+            toast.error("It appears that our system is currently unresponsive. Please try again later.");
+            return;
+        }
+
+        const responseJSON = await response.json();
+
+        if (responseJSON?.error) {
+            toast.error(responseJSON.error);
+            return;
+        } else {
+            toast.success(responseJSON.message);
+            const bookmarks = responseJSON.bookmarks;
+            const shortcuts = responseJSON.shortcuts;
             props.setBookmarks({ ...bookmarks });
             props.setShortcuts(shortcuts);
         }
