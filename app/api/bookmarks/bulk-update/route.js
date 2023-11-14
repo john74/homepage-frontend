0923c0ee3a539fccb1c383f79ffe55b438127313
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-14 19:17:09 +0100

    refactor: This commit removes the existing logic from app/api/bookmarks/bulk-update/ and replaces it with calls to the handleApiRequest function.

--- app/api/bookmarks/bulk-update/route.js
+++ app/api/bookmarks/bulk-update/route.js
@@ -1,40 +1,6 @@
-import { refreshAccessToken } from "@lib";
+import { handleApiRequest } from "@lib";
 
 
 export async function PUT(request) {
-    let accessToken = request.cookies.get("accessToken")?.value;
-    if (!accessToken) {
-      accessToken = await refreshAccessToken(request);
-    }
-
-    const body = await request.json();
-
-    const initOptions = {
-        cache: 'no-store',
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            authorization: `JWT ${accessToken}`,
-        },
-        body: JSON.stringify(body)
-    };
-
-    let response = await fetch(
-        'http://127.0.0.1:8000/api/bookmarks/bulk-update/',
-        initOptions,
-    )
-    .catch(error => {
-        return {error: error}
-    })
-
-    if (response?.error || response?.status == 500) {
-        return Response.json({error: "It appears that our system is currently unresponsive. Please try again later."});
-    }
-
-    const responseJSON = await response.json();
-    if (response?.status != 200) {
-        return Response.json({error: responseJSON.error});
-    }
-
-    return Response.json(responseJSON);
+    return await handleApiRequest(request);
 }
