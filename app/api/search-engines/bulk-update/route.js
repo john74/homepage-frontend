765fa8eb67bed33e756c11a5c342dc0bc14528fa
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-14 16:35:54 +0100

    refactor: This commit removes the existing logic from app/api/search-engines/bulk-update/ and replaces it with calls to the handleApiRequest function.

--- app/api/search-engines/bulk-update/route.js
+++ app/api/search-engines/bulk-update/route.js
@@ -1,39 +1,6 @@
-import { refreshAccessToken } from "@lib";
+import { handleApiRequest } from "@lib";
 
 
 export async function PUT(request) {
-    let accessToken = request.cookies.get("accessToken")?.value;
-    if (!accessToken) {
-      accessToken = await refreshAccessToken(request);
-    }
-
-    const data = await request.json();
-    const initOptions = {
-        cache: 'no-store',
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json",
-            authorization: `JWT ${accessToken}`,
-        },
-        body: JSON.stringify(data)
-    };
-
-    let response = await fetch(
-        process.env.BACKEND_SEARCH_ENGINES_UPDATE_URL,
-        initOptions,
-    )
-    .catch(error => {
-        return {error: error}
-    })
-
-    if (response?.error || response?.status == 500) {
-        return Response.json({error: "It appears that our system is currently unresponsive. Please try again later."});
-    }
-
-    const responseJSON = await response.json();
-    if (response?.status != 200) {
-        return Response.json({error: responseJSON.error});
-    }
-
-    return Response.json(responseJSON);
+    return await handleApiRequest(request);
 }
