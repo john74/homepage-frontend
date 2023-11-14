a11b6b8d048ec40ae39c3d3106f16775c0a5316b
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-14 16:15:48 +0100

    feat: This commit introduces the useHandleProxyRequest hook, designed to handle responses from frontend endpoints. The hook encapsulates common logic for making API requests, processing responses, and displaying error and success toast messages. It aims to eliminate redundancy by providing a centralized approach for handling frontend endpoint interactions.

--- hooks/index.js
+++ hooks/index.js
@@ -1,6 +1,9 @@
+"use client";
+
 export { default as useSignUp } from './useSignUp';
 export { default as useSignIn } from './useSignIn';
 export { default as useMarkForDeletion } from './useMarkForDeletion';
 export { default as useToggleMenu } from './useToggleMenu';
 export { default as useFormVisibility } from './useFormVisibility';
 export { default as useSelectSearchEngine } from './useSelectSearchEngine';
+export { default as useHandleProxyRequest } from './useHandleProxyRequest';
