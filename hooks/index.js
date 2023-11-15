9bb5f228b1a7fc975fb92c3847e46acf1d3bf3d1
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-15 12:05:21 +0100

    refactor: Remove useSignIn hook as its functionality has been integrated directly into the sign-in page.

--- hooks/index.js
+++ hooks/index.js
@@ -1,6 +1,5 @@
 "use client";
 
-export { default as useSignIn } from './useSignIn';
 export { default as useMarkForDeletion } from './useMarkForDeletion';
 export { default as useToggleMenu } from './useToggleMenu';
 export { default as useFormVisibility } from './useFormVisibility';
