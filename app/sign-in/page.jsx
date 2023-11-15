cc84174093103b5ec7aa56c5edd1793e24528b96
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-15 13:00:56 +0100

    refactor: Consolidate logic from useSignIn hook into the sign-in page.

--- app/sign-in/page.jsx
+++ app/sign-in/page.jsx
@@ -1,15 +1,42 @@
 "use client";
+
 import styles from '../../styles/Forms.module.css';
+import { useState } from "react";
 import Link from 'next/link';
-import { useSignIn } from "@hooks";
 import { redirect } from 'next/navigation';
+import { signIn } from "next-auth/react";
+import { toast } from 'react-hot-toast';
 
 
 const SignInPage = () => {
-    const {
-        email, password, signInSuccess,
-        onChange, onSubmit
-    } = useSignIn();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    })
+
+    const { email, password } = formData;
+
+    const onChange = event => {
+        const {name, value} = event.target;
+        setFormData({ ...formData, [name]:value })
+    }
+
+    const [signInSuccess, setSignInSuccess] = useState(false);
+    const login = async () => {
+        const response = await signIn("credentials", {
+            email: email,
+            password: password,
+            redirect: false,
+            callbackUrl: "/"
+        });
+
+        if (response?.error) {
+            toast.error(response.error);
+        } else {
+            setSignInSuccess(true);
+            toast.success("Login successful.");
+        }
+    };
 
     if (signInSuccess) {
         redirect('/');
@@ -28,7 +55,7 @@ const SignInPage = () => {
                         <label className={styles.label} htmlFor="password">Password:</label>
                         <input className={styles.input} type="password" id="password" name="password" onChange={onChange}/>
                     </div>
-                    <input className={styles.input} type="submit" value="Sign in" onClick={onSubmit}/>
+                    <input className={styles.input} type="submit" value="Sign in" onClick={login}/>
                     <div className={styles.authOptions}>
                         <span className={styles.text}>Don't have an account?</span>
                         <Link className={styles.link} href="/sign-up/">Sign up</Link>
