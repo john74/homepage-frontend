ab72808401b697fcd21d011161807737f5365419
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-16 20:44:35 +0100

    refactor: Eliminate redundant refreshAccessToken callback in authOptions, as token refreshing is now delegated to the lib/refreshAccessToken function.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -73,20 +73,6 @@ export const authOptions = {
             return user;
          },
 
-        async refreshAccessToken() {
-            const refreshToken = cookies().get('refreshToken')?.value;
-            if (!refreshToken) return;
-            const initOptions = {
-                cache: 'no-store',
-                method: "POST",
-                headers: {'Content-Type': 'application/json'},
-                body: JSON.stringify({"refresh": refreshToken})
-            }
-            let response = await fetch(`${process.env.BACKEND_REFRESH_TOKEN_URL}`, initOptions);
-            let accessTokenData = await response.json();
-            return accessTokenData.access;
-        },
-
         async jwt({ token, user, trigger, session }) {
             return { ...token, ...user };
         },
