4791fa8c934d375399a729d504236baaffd195ec
JohnEf <eustathiou.ioan@protonmail.com>
2023-11-28 18:08:11 +0100

    feat: Implement 'Add New Category' option in BookmarkCategoryGroups component, introduce functionality to support category addition, and incorporate corresponding styles.

--- components/BookmarkCategoryGroups.jsx
+++ components/BookmarkCategoryGroups.jsx
@@ -2,10 +2,32 @@ import styles from '../styles/BookmarkCategoryGroups.module.css';
 import {
     CategoriesGroup, EmptyGroups,
 } from './BookmarkParts';
+import {
+    useHandleProxyRequest,
+} from '@hooks';
 
 
 function BookmarkCategoryGroups(props) {
     const groups = props.bookmarkCategoryGroups;
+
+    const createBookmarkCategory = async (event) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const method = "POST";
+        const targetEndpoint = "api/categories/bulk-create/";
+        const url = `${props.baseUrl}/api/${method.toLowerCase()}/?targetEndpoint=${targetEndpoint}`;
+        const body = [{
+            "name": "New Category",
+            "color": "#fff"
+        }];
+
+        const responseJSON = await useHandleProxyRequest(url, method, body,);
+        if (!responseJSON) return;
+
+        const categories = responseJSON.categories;
+        props.setBookmarkCategoryGroups(categories);
+    };
 
     if (!groups?.length) {
         return <EmptyGroups styles={styles} {...props} />;
@@ -15,9 +37,10 @@ function BookmarkCategoryGroups(props) {
         <>
         <div className={styles.bookmarkCategoryGroups}>
             <div className={styles.wrapper}>
-            {groups.map((group, index) => (
-                <CategoriesGroup key={`bookmark-category-group-${index}`} styles={styles} group={group} {...props} />
-            ))}
+                <p onClick={createBookmarkCategory}>Add new category</p>
+                {groups.map((group, index) => (
+                    <CategoriesGroup key={`bookmark-category-group-${index}`} styles={styles} group={group} {...props} />
+                ))}
             </div>
         </div>
         </>
