353093c1d2d2815c038317d4c60ec6ba767e7e17
JohnEf <eustathiou.ioan@protonmail.com>
2023-12-19 15:28:00 +0100

    chore: Encapsulate icons inside React fragment

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -2,11 +2,13 @@ function DropdownArrow(props) {
     const styles = props.styles;
 
     return (
+        <>
         <span className={styles.icon} onClick={(event) => (props.onClick ? props.onClick(event, props.id) : null)}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <path d="m6 9 6 6 6-6"/>
             </svg>
         </span>
+        </>
     )
 }
 
@@ -14,13 +16,15 @@ function MenuIcon(props) {
     const styles = props.styles;
 
     return (
+        <>
         <span className={styles.icon} onClick={(event) => (props.onClick ? props.onClick(event, props.id) : null)}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
                 <line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/>
             </svg>
         </span>
+        </>
     )
 }
 
 
-export { DropdownArrow, MenuIcon};
+export { DropdownArrow, MenuIcon };
