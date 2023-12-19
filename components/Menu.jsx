78062dfd7b8a35511b9c371d00d9e5eadd01d1c9
JohnEf <eustathiou.ioan@protonmail.com>
2023-12-19 15:20:16 +0100

    refactor: Use Dropdown component

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -1,5 +1,5 @@
 import {
-    DropdownArrow, Dropdown, MenuIcon
+    Dropdown, MenuIcon
 } from '@components';
 
 
@@ -8,20 +8,13 @@ function Menu(props) {
     const styles = props.styles;
     const {
         toggleMenu,
-        openMenuId,
     } = props.toggleMenuHook;
 
     return (
         <>
         <div className={styles.menu}>
             <MenuIcon styles={styles} onClick={toggleMenu} id={props.id} {...props}/>
-            <ul className={`${styles.dropdown} ${openMenuId === props.id ? styles.open : ''}`}>
-            {options.map((option, index) => (
-                <li key={option?.id+option?.name+index} className={styles.option} onClick={(event) => option.onClick(event)}>
-                {option.name}
-                </li>
-            ))}
-            </ul>
+            <Dropdown styles={styles} options={options} {...props}/>
         </div>
         </>
     );
